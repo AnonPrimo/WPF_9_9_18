8e41ca9414f1addcc708f6ca97596ceb44b033c1
wispeckiy <max.vitvitckiy@gmail.com>
2018-09-09 20:22:28 +0300

    Добавьте файлы проекта.

---
+++ WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            cm.Items.Add(0);
+            cm.Items.Add(5);
+            cm.Items.Add(15);
+            cm.Items.Add(30);
+            cm.Items.Add(45);
+            cm.Items.Add(60);
+            cm.Items.Add(90);
+            cm.Items.Add(120);
+            cm.Items.Add(150);
+            cm.Items.Add(180);
+            cm.Items.Add(200);
+            cm.Items.Add(250);
+
+        }
+    }
+}
